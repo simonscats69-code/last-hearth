f07e1cc2be9043149ce7688ea909992b5e88d767
SimonsCat <simonscat@yandex.ru>
2026-03-10 23:08:04 +0300

    Debug: list files in container

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 
+echo "=== Список файлов в /app ==="
+ls -la /app
+echo "=== Конец списка ==="
+
 mkdir -p /app/data
 chmod 777 /app/data
 chown -R $(id -u):$(id -g) /app/data 2>/dev/null || true
